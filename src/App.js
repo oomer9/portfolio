5e3589b1b47f7a68258690aaf794dd26f4b1081e
Omer Omer <omersquared94@gmail.com>
2020-09-12 22:46:12 -0400

    start

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
+import Home from './home/home.tsx'
+
+
+function App() {
+  return (
+    <div>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/home/home.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './home.scss';
+import '../style/globalstyles.scss';
+
+
+const Home = () =>{
+
+
+    return(
+        <div className="home">
+            <div className=" d-flex flex-row justify-content-center">
+                <div className="header">Omer Omer</div>
+            </div>
+        </div>
+    );
+
+}
+
+export default Home;
