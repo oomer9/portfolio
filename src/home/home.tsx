b1f68bc5ba943b692fd0e91d4aa57628a926a502
Omer Omer <omersquared94@gmail.com>
2020-09-19 15:58:53 -0400

    add new svg and start wit navBar

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
-import './home.scss';
+import classes from  './home.module.scss';
 import '../style/globalstyles.scss';
 import Button from '../uicomponents/button/button';
-
+import blackHole from '../svgs/blackHole.svg';
 
 class Home extends Component{
 
     render(){
     return(
-        <div className="home">
+        <div className={classes.home}>
             <div className=" d-flex flex-row justify-content-center">
-                <div className="header">---- Omer Omer ----</div>
+                <img className={classes.flourishleft} src={blackHole} />  
+                <div className={classes.header}>Omer Omer</div>
+                <img className={classes.flourishIcon} src={blackHole} />            
+            </div>
+            <div className={` d-flex flex-row justify-content-center align-items-center ${classes.navBar}`}>
+                <Button className=" mr-2" label="About Me" />
+                <Button label="Graphs with React" />
             </div>
         </div>
     )};
 }
 
 export default Home;
+
--- src/uicomponents/button/button.tsx
+++ src/uicomponents/button/button.tsx
@@ -5,13 +5,13 @@ import './button.scss';
 const Button: any = ({
 label = null,
 type= " primary ",
+className="",
 onClick = () => true
 }) =>{
-    const className = type
-
+    const localClassName = ` ${type} ${className}`
     return(
         <div>
-            <MaterialButton onClick={onClick} className={className} variant="contained">{label}</MaterialButton>
+            <MaterialButton onClick={onClick} className={localClassName} >{label}</MaterialButton>
         </div>
     );
 }
