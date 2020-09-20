25a2c78f811f67f0b81c7cae80a8ea6c2c5389c1
Omer Omer <omersquared94@gmail.com>
2020-09-20 09:36:29 -0400

    new layout

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -4,34 +4,36 @@ import '../style/globalstyles.scss';
 import './home.scss'
 import Button from '../uicomponents/button/button';
 import blackHole from '../svgs/blackHole.svg';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
+import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
+import HomeMenu from '../uicomponents/menu/menu';
   
 class Home extends Component{
 
+    
     render(){
+        const menuOptions=[{label:"About",route:"/about"},{label:"Graphs with React",route:"/graphs"}];
     return(
         <div className={classes.home}>
             <Router>
-            <div className=" d-flex flex-row justify-content-center">
-                <img className={classes.flourishleft} src={blackHole} />  
-                <Link to="/"><div className={classes.header}>Omer Omer</div></Link>
-                <img className={classes.flourishIcon} src={blackHole} />            
-            </div>
-            
-                <div className={` d-flex flex-row justify-content-center align-items-center ${classes.navBar}`}>
-                    <Link to="/about"><Button className=" mr-2" label="About Me" /></Link>
-                    <Link to="/graphs"><Button label="Graphs with React" /></Link>
-                </div>
-                <Switch>
-                    <Route  exact path="/">
-                        <div className="  d-flex flex-row justify-content-center ">
+                <div className={classes.top}>
+                    <div className={" d-flex flex-row justify-content-between align-items-center "}>
+                        <div></div>
+                        <div >
+                            <img className={classes.headerIconLeft} src={blackHole}/>
+                            <Link to="/"><span className={classes.header}>Omer Omer</span></Link>
+                            <img className={classes.headerIcon}  src={blackHole}/>
                         </div>
-                    </Route>
+                        <div className=" mb-3 " ><HomeMenu menuOptions={menuOptions} />  </div>
+                    </div>
+                    <div className={classes.headerLine}></div>
+                </div>
+            <Switch>
+                    <div className={classes.content}>
+                        <Route  exact path="/">
+                            <div >
+                            </div>
+                        </Route>
+                    </div>
                 </Switch>
             </Router>
         </div>
---
+++ src/uicomponents/menu/menu.tsx
@@ -0,0 +1,43 @@
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import React from 'react'
+import MenuIcon from '../../svgs/menu.svg'
+import './menu.scss';   
+import classes from './menu.module.scss';
+import {BrowserRouter as Router,Link} from "react-router-dom";
+
+const HomeMenu: any = ({
+className=" ",
+menuOptions=[{label:"",route:""}],
+})=>{
+    const [anchor, setAnchor] = React.useState<null | HTMLElement>(null);
+    const handleClick = (event: any) => {
+        setAnchor(event.currentTarget);
+      };
+    
+      const handleClose = () => {
+        setAnchor(null);
+      };
+
+    return(
+        <div className={className}>
+            <div className={classes.icon} onClick={(e)=>{handleClick(e)}}><img  src={MenuIcon} /></div>  
+            <Menu
+            id="simple-menu"
+            anchorEl={anchor}
+            keepMounted
+            open={Boolean(anchor)}
+            onClose={handleClose}
+             >
+                 <Router>
+                {menuOptions && menuOptions.map((item)=>{
+                  return(
+                    <Link to={item.route}><MenuItem onClick={handleClose}>{item.label}</MenuItem></Link>
+                  )  
+                })}
+                </Router>
+            </Menu>
+        </div>
+    )
+}
+export default HomeMenu
