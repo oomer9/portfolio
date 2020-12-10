6fb7ff62834eef4b2ad820ec70f4f680b8f9fef1
Omer Omer <omersquared94@gmail.com>
2020-12-10 08:43:11 -0500

    git hub pages push

--- src/datarendering/datarendering.tsx
+++ src/datarendering/datarendering.tsx
@@ -4,37 +4,42 @@ import data from '../services/data.json';
 import {AgGridReact} from 'ag-grid-react';
 import classes from './dataRendering.module.scss'
 import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-blue.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 import Dialog from '../uicomponents/dialog/dialog';
 
 const DataRendering =()=>{
     const [colDefs,setColDefs] = useState([{}]);
     const [rowData,setRowData] = useState(data);
+    const [gridApi, setGridApi] = useState(null);
+    const [gridColumnApi, setGridColumnApi] = useState(null);
 
     useEffect(() => {
         setColumnDefs();
         console.log(data)
       },[]);
 
+      const onGridReady = (params:any) => {
+        setGridApi(params.api);
+        setGridColumnApi(params.columnApi);
+    }   
+ 
       const setColumnDefs = ()=>{
         const columnDefs =    [
             { headerName: 'Title', field: 'title',width:300 },
             { headerName: 'Year Released', field: 'year' },
-            { headerName: 'Genre', field: 'genre',width:300}
+            { headerName: 'Genre', field: 'genre'}
         ];
         setColDefs(columnDefs);
     }
-    const rowClicked = (e: any)=>{
-        console.log(e)
-    }
+   
     return(
         <div>
             <div className=" d-flex flex-row justify-content-center ">
                 Movies 
             </div>
-            <div className={classes.grid + "  justify-content-center ag-theme-blue mb-4 "}> 
+            <div className={classes.grid + " ag-theme-alpine mb-4 "}> 
                 <AgGridReact rowData={rowData}
-                columnDefs={colDefs} onRowClicked={rowClicked} enableBrowserTooltips={true} >
+                columnDefs={colDefs} onGridReady={onGridReady} onRowClicked={rowClicked} enableBrowserTooltips={true} >
                 </AgGridReact>
             </div>
             <Dialog isOpen={false}>
@@ -42,8 +47,9 @@ const DataRendering =()=>{
             </Dialog>
         </div>
     )
+};
+    const rowClicked = (e: any)=>{
+        console.log(e)
+    }
     
-
-   
-}
 export default DataRendering;
