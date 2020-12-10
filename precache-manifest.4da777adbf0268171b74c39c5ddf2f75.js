990b5dbd5ed247de3e0177f7c897ab84b26896e1
Omer Omer <omersquared94@gmail.com>
2020-12-10 08:44:38 -0500

    Updates

---
+++ precache-manifest.4da777adbf0268171b74c39c5ddf2f75.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e459b666e7497f8f18baf3c660fa157e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4c2672f0e59463ce300a",
+    "url": "/portfolio/static/css/2.38666771.chunk.css"
+  },
+  {
+    "revision": "52ec5405f1bb533d55fb",
+    "url": "/portfolio/static/css/main.6feadfb1.chunk.css"
+  },
+  {
+    "revision": "4c2672f0e59463ce300a",
+    "url": "/portfolio/static/js/2.fe837a3e.chunk.js"
+  },
+  {
+    "revision": "3879d6e403b093ce8df1bb0ef6791c29",
+    "url": "/portfolio/static/js/2.fe837a3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52ec5405f1bb533d55fb",
+    "url": "/portfolio/static/js/main.f0e2401b.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "b8c4861c4fc1b6917b37a7e1a255d057",
+    "url": "/portfolio/static/media/blackHole.b8c4861c.svg"
+  },
+  {
+    "revision": "4837c2c31e4994dbad6e64aacc1aeae1",
+    "url": "/portfolio/static/media/menu.4837c2c3.svg"
+  },
+  {
+    "revision": "ed8ab84152ac9aa62ac80fdf66a00056",
+    "url": "/portfolio/static/media/omerPic.ed8ab841.jpeg"
+  }
+]);
