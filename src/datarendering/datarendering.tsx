4632ea6307f66d08079f493726c0e7112c4de5d8
Omer Omer <omersquared94@gmail.com>
2020-12-14 14:46:18 -0500

    update grid

--- src/datarendering/datarendering.tsx
+++ src/datarendering/datarendering.tsx
@@ -4,18 +4,21 @@ import data from '../services/data.json';
 import {AgGridReact} from 'ag-grid-react';
 import classes from './dataRendering.module.scss'
 import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+import 'ag-grid-community/dist/styles/ag-theme-blue.css';
 import Dialog from '../uicomponents/dialog/dialog';
+import Button from '../uicomponents/button/button';
+import './dataRendering.scss';
 
 const DataRendering =()=>{
     const [colDefs,setColDefs] = useState([{}]);
     const [rowData,setRowData] = useState(data);
     const [gridApi, setGridApi] = useState(null);
     const [gridColumnApi, setGridColumnApi] = useState(null);
+    const [movieImage,setMovieImage] = useState("");
+    const [openDialog, setOpenDialog] = useState(false)
 
     useEffect(() => {
         setColumnDefs();
-        console.log(data)
       },[]);
 
       const onGridReady = (params:any) => {
@@ -25,34 +28,48 @@ const DataRendering =()=>{
  
       const setColumnDefs = ()=>{
         const columnDefs =    [
-            { headerName: 'Title', field: 'title',resizable: true },
-            { headerName: 'Year Released', field: 'year',resizable: true },
-            { headerName: 'Genre', field: 'genre',resizable: true}
+            { headerName: 'Title', field: 'title',resizable: true, width:320},
+            { headerName: 'Year Released', field: 'year',resizable: true, width:160 },
+            { headerName: 'Genre', field: 'genre',resizable: true, width:300}
         ];
         setColDefs(columnDefs);
     }
-   
+    const rowClicked = (e: any)=>{
+        if(e.data && e.data.img_url){
+            setOpenDialog(true);
+            setMovieImage(e.data.img_url)
+        }
+        
+    }
+    const closeDialog=()=>{
+        setOpenDialog(false);
+        setMovieImage("");
+    }
     return(
         <div >
             <div className=" d-flex flex-row justify-content-center ">
                 Movies 
             </div>
             <div className=" d-flex justify-content-center  ">
-                <div className={classes.grid + "  ag-theme-alpine mb-4 "}> 
+                <div className={classes.grid + "  ag-theme-blue mb-4 "}> 
                     <AgGridReact rowData={rowData}
-                    columnDefs={colDefs} onGridReady={onGridReady} onRowClicked={rowClicked} enableBrowserTooltips={true} >
+                    columnDefs={colDefs} onGridReady={onGridReady} onRowClicked={rowClicked}  >
                     </AgGridReact>
                 </div>
             </div>
             
-            <Dialog isOpen={false}>
+            <Dialog isOpen={openDialog} onClose={closeDialog} header="Movie Clicked">
+                <div className=" d-flex flex-row justify-content-center mb-2 ">
+                    <img src={movieImage} alt="Error Finding Movie image"/>
+                </div>
+                <div className=" d-flex flex-row justify-content-center ">
+                    <Button label="OK" onClick={closeDialog}/>
+                </div>
                 
             </Dialog>
         </div>
     )
 };
-    const rowClicked = (e: any)=>{
-        console.log(e)
-    }
+    
     
 export default DataRendering;
